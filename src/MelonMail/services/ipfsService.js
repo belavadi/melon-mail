7de971c0b8428af54d6c5c1ed2a356b42f034c7f
nikolavukovic <sterlu.sd@gmail.com>
2017-09-13 17:11:36 +0200

    Replicate new files and objects to predefined nodes

--- src/MelonMail/services/ipfsService.js
+++ src/MelonMail/services/ipfsService.js
@@ -1,51 +1,94 @@
 const IPFS = require('ipfs');
+const ipfsAPI = require('ipfs-api');
 const concat = require('concat-stream');
 
-const ipfsNodesList = [
+const ipfsBootstrapNodesList = [
   '/ip4/127.0.0.1/tcp/9999/ws/ipfs/QmWc6qiH7rp7i8J2RcKvWgaw9daTW5LipfaBWbNkSuLTuU',
   '/ip4/46.101.79.241/tcp/9999/ws/ipfs/QmNxpsbNJzvXpUbv9Kp9YnNAdgSzNz8DGWay8ie7pyLy5q',
+];
+
+const ipfsNodesList = [
+  ['/ip4/127.0.0.1/tcp/5001'],
+  ['ipfs.decenter.com', '443', { protocol: 'https' }],
 ];
 
 const ipfsNode = new IPFS({
   repo: 'ipfs-melon',
   config: {
-    Bootstrap: ipfsNodesList,
+    Bootstrap: ipfsBootstrapNodesList,
   },
 });
 
-const uploadData = (data) => {
-  const preparedData = new Buffer(JSON.stringify(data));
-
-  return ipfsNode.files.add(preparedData);
+const replicate = (hash, type) => {
+  console.log(`Replicating ${type} with hash: ${hash}`);
+  let successful = 0;
+  const replicationPromises = ipfsNodesList.map(node =>
+    new Promise((resolve) => {
+      const api = ipfsAPI(...node);
+      return (type === 'file' ? api.files.get(hash) : api.object.get(hash))
+        .then(() => {
+          successful += 1;
+          resolve();
+        })
+        .catch(() => resolve());
+    }),
+  );
+  Promise.all(replicationPromises)
+    .then(() => console.log(`Successfully replicated ${type} with hash: ${hash} on ${successful}/${ipfsNodesList.length} nodes`));
 };
 
-const newThread = (mail) => {
-  if (!mail.hash || !mail.size) {
-    return false;
-  }
+const uploadData = data =>
+  new Promise((resolve, reject) => {
+    const preparedData = new Buffer(JSON.stringify(data));
 
-  const thread = {
-    Data: new Buffer(''),
-    Links: [{
-      ...mail,
-      name: '0',
-    }],
-  };
+    return ipfsNode.files.add(preparedData)
+      .then((res) => {
+        replicate(res[0].hash, 'file');
+        resolve(res);
+      })
+      .catch(err => reject(err));
+  });
 
-  return ipfsNode.object.put(thread);
-};
+const newThread = mail =>
+  new Promise((resolve, reject) => {
+    if (!mail.hash || !mail.size) {
+      resolve(false);
+    }
+
+    const thread = {
+      Data: new Buffer(''),
+      Links: [{
+        ...mail,
+        name: '0',
+      }],
+    };
+
+    return ipfsNode.object.put(thread)
+      .then((res) => {
+        replicate(res.toJSON().multihash, 'object');
+        resolve(res);
+      })
+      .catch(err => reject(err));
+  });
 
 const replyToThread = (reply, threadHash) =>
-  ipfsNode.object.get(threadHash)
-    .then((thread) => {
-      const parsedThread = thread.toJSON();
-      const replyLink = {
-        ...reply,
-        multihash: reply.hash,
-        name: parsedThread.links.length,
-      };
-      return ipfsNode.object.patch.addLink(threadHash, replyLink);
-    });
+  new Promise((resolve, reject) =>
+    ipfsNode.object.get(threadHash)
+      .then((thread) => {
+        const parsedThread = thread.toJSON();
+        const replyLink = {
+          name: `${parsedThread.links.length}`,
+          size: reply.size,
+          multihash: reply.hash,
+        };
+        return ipfsNode.object.patch.addLink(thread.toJSON().multihash, replyLink)
+          .then((res) => {
+            replicate(res.toJSON().multihash, 'object');
+            resolve(res);
+          })
+          .catch(err => reject(err));
+      }),
+  );
 
 const getThread = hash => ipfsNode.object.get(hash);
 
@@ -68,4 +111,5 @@ export default {
   getFile,
   getFileStream,
   getFileContent,
+  replicate,
 };
