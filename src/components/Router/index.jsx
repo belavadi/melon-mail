bef4c3cd226ff3db8780a45ed8f2648a3aa1205f
Mateja Trifunovski <mtrifunovski@gmail.com>
2018-01-17 12:02:03 +0100

    Refactor Router to a stateless component

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,46 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { logout, changeAccount, changeNetwork, setAccount } from '../../actions/auth';
-import { getBalance, saveLastActiveTimestamp } from '../../actions/utility';
-import eth from '../../services/ethereumService';
 
 import Auth from '../Auth/';
 import App from '../Mail/App/';
 
-class Router extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-
-    this.getCurrentInfo = this.getCurrentInfo.bind(this);
-  }
-
-  componentWillMount() {
-    this.getCurrentInfo();
-    setInterval(this.getCurrentInfo, 1000);
-  }
-
-  getCurrentInfo() {
-  }
-
-  render() {
-    return (
-      <div className="melonmail-wrapper">
-        {
-          this.props.router.path === '/' &&
-          <App />
-        }
-        {
-          this.props.router.path === 'auth' &&
-          <Auth />
-        }
-      </div>
-    );
-  }
-}
+const Router = ({ router }) => (
+  <div className="melonmail-wrapper">
+    {
+      router.path === '/' &&
+      <App />
+    }
+    {
+      router.path === 'auth' &&
+      <Auth />
+    }
+  </div>
+);
 
 Router.propTypes = {
   router: PropTypes.shape({
@@ -51,14 +28,7 @@ Router.propTypes = {
 Router.defaultProps = {};
 
 const mapStateToProps = state => state;
-const mapDispatchToProps = dispatch => bindActionCreators({
-  logout,
-  changeAccount,
-  getBalance,
-  setAccount,
-  changeNetwork,
-  saveLastActiveTimestamp,
-}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
 
 export default connect(
   mapStateToProps,
